4429777d4c36ff5c3779c7fa892e17459c4a3687
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-4.local>
2019-11-03 13:21:07 -0500

    addAll works

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.phonebook;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,32 +9,45 @@ import java.util.Map;
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+    private Map<String, List<String>> map;
+
     public PhoneBook(Map<String, List<String>> map) {
+        this.map = map;
+
     }
 
     public PhoneBook() {
-        this(null);
+        this.map = new HashMap<>();
     }
 
     public void add(String name, String phoneNumber) {
+        ArrayList<String> currentNumbers = new ArrayList<>();
+        currentNumbers.add(phoneNumber);
+        map.put(name, currentNumbers);
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        ArrayList<String> currentNumbers = new ArrayList<>();
+        for(String phoneNumber : phoneNumbers){
+            currentNumbers.add(phoneNumber);
+            map.put(name,currentNumbers);
+        }
     }
 
     public void remove(String name) {
+        map.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        return map.containsValue(name);
     }
 
     public List<String> lookup(String name) {
-        return null;
+        return map.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
-        return null;
+    return null;
     }
 
     public List<String> getAllContactNames() {
@@ -40,6 +55,6 @@ public class PhoneBook {
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return map;
     }
 }
